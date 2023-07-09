d4d412edc71491ea186f0b3e43f1c108ce4ae209
eugen1701 <eugen.flocea@yahoo.com>
2023-07-09 23:57:10 +0300

    Make verifyId work

--- src/main/java/com/app/esncr/restfulservice/controller/EsncrController.java
+++ src/main/java/com/app/esncr/restfulservice/controller/EsncrController.java
@@ -1,9 +1,15 @@
 package com.app.esncr.restfulservice.controller;
 
+import com.app.excel.HandleExcel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EsncrController {
 
-
+    @GetMapping("/verify-id/{idToVerify}")
+    public boolean verifyId(@PathVariable final String idToVerify) {
+        return HandleExcel.verifyIdIsPresent(idToVerify);
+    }
 }
--- src/main/java/com/app/excel/HandleExcel.java
+++ src/main/java/com/app/excel/HandleExcel.java
@@ -2,20 +2,19 @@ package com.app.excel;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Iterator;
 
 public class HandleExcel {
 
-    public String pathFile = "C:\\dev\\esncr\\src\\main\\resources\\dummy.xlsx";
+    static public String pathFile = "C:\\dev\\esncr\\src\\main\\resources\\dummy.xlsx";
 
-    public boolean verifyIdIsPresent(String idToVerify) {
+    static public boolean verifyIdIsPresent(String idToVerify) {
         try {
             File file = new File(pathFile);
             FileInputStream fileInputStream = new FileInputStream(file);
@@ -23,14 +22,21 @@ public class HandleExcel {
             XSSFSheet sheet = workbook.getSheetAt(0);
             for (Row row : sheet) {
                 Cell cell = row.getCell(0); // get the first cell of the row (assuming ID is in the first column)
-                if (cell != null && cell.getCellType() == CellType.STRING && idToVerify.equals(cell.getStringCellValue())) {
+                CellType type = cell.getCellType();
+                if(type == CellType.NUMERIC) {
+                    DataFormatter formatter = new DataFormatter();
+                    String stringCell = formatter.formatCellValue(cell);
+                    if(stringCell.equals(idToVerify))
+                        return true;
+                }
+                if (type == CellType.STRING && cell.getStringCellValue().startsWith(idToVerify)) {
                     return true;
                 }
             }
 
 
         }catch (Exception exception){
-
+            System.out.println(exception);
         }
         return false;
     }
